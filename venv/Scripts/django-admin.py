522d5a15a11dcc792fa9ee5e4b5744573e74475d
eminarroDAM <41004702a@iespoblenou.org>
2022-05-07 00:29:45 +0200

    Login and Logout

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Eric\Documents\DAMPoblenou\2n\M13\ChatServerPlayground\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
